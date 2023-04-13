dce6efcac0365e5c0edbbf0b056a381791f319cc
kukuh_wildan <kuwildansyah@gmail.com>
2023-04-13 12:26:39 +0700

    + latihan 1 dan kegiatan 1

--- src/Latihan1.java
+++ src/Latihan1.java
@@ -1,11 +1,11 @@
 import java.util.HashMap;
 import java.util.Scanner;
 
-public class Main {
+public class Latihan1 {
 
         String nama,kelas, matkulPerkuliahan;
         int nim;
-            public Main (String nm, String kl, String mat, int ni){
+            public Latihan1(String nm, String kl, String mat, int ni){
             nama=nm;
             kelas = kl;
             matkulPerkuliahan = mat;
@@ -13,13 +13,13 @@ public class Main {
 
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        HashMap<String, Main> mhs = new HashMap<>();
+        HashMap<String, Latihan1> mhs = new HashMap<>();
         String input;
-        Main data;
+        Latihan1 data;
 
-        mhs.put("1", new Main("putri","3H","Struktur Data", 2020000));
-        mhs.put("2", new Main("agus","3A","Struktur Data", 2020001));
-        mhs.put("3", new Main("Arro","3D","Struktur Data", 2020002));
+        mhs.put("1", new Latihan1("putri","3H","Struktur Data", 2020000));
+        mhs.put("2", new Latihan1("agus","3A","Struktur Data", 2020001));
+        mhs.put("3", new Latihan1("Arro","3D","Struktur Data", 2020002));
 
         System.out.println("Masukkan ID :");
         input= in.nextLine();
